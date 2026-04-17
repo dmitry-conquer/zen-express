f17de8b149b73d98ce49fe01587054fc685352b6
Dmytro Frolov <dmitry.conquer@gmail.com>
2026-04-17 13:56:17 +0300

    Enhance theme setup by adding comments disabling functionality and updating SFTP configuration
    - Updated SFTP configuration to ignore 'acf-json' directory.
    - Added 'disable_comments' method in Setup class to disable comments across the theme:
      - Redirects users from comments page in admin.
      - Removes comments metabox from dashboard.
      - Disables comments and trackbacks for all post types.
      - Closes comments on the front-end and hides existing comments.
      - Removes comments page from admin menu and links from admin bar.

--- wp-theme/inc/Setup.php
+++ wp-theme/inc/Setup.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace WP_Starter_Theme;
+
 if (!defined('ABSPATH')) {
   exit;
 }
 
-final class Setup
-{
-  public static function register()
-  {
+final class Setup {
+  public static function register() {
     add_action('after_setup_theme', [self::class, 'register_menus']);
     add_action('after_setup_theme', [self::class, 'setup_theme']);
     add_action('wp_enqueue_scripts', [self::class, 'remove_block_css']);
@@ -15,10 +15,10 @@ final class Setup
     add_action('login_headerurl', [self::class, 'custom_login_logo_url']);
     add_action('login_headertext', [self::class, 'custom_login_logo_url_title']);
     add_filter('wp_img_tag_add_auto_sizes', '__return_false');
+    self::disable_comments();
   }
 
-  public static function register_menus()
-  {
+  public static function register_menus() {
     register_nav_menus([
       'header_menu' => __('Header menu'),
       'header_mobile_menu' => __('Header mobile menu'),
@@ -26,16 +26,14 @@ final class Setup
     ]);
   }
 
-  public static function remove_block_css()
-  {
+  public static function remove_block_css() {
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('wp-block-library-theme');
     wp_dequeue_style('wc-block-style');
     wp_dequeue_style('storefront-gutenberg-blocks');
   }
 
-  public static function setup_theme()
-  {
+  public static function setup_theme() {
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
     add_theme_support('custom-logo', [
@@ -46,15 +44,13 @@ final class Setup
     ]);
   }
 
-  public static function allow_custom_mime_types()
-  {
+  public static function allow_custom_mime_types() {
     $mimes['woff'] = 'font/woff';
     $mimes['woff2'] = 'font/woff2';
     return $mimes;
   }
 
-  public static function custom_login_styles()
-  {
+  public static function custom_login_styles() {
     echo '<style>
 			body.login {
 					background: linear-gradient(357deg, rgb(70 182 191 / 67%) 0%, rgb(255 255 255 / 82%) 100%);
@@ -72,13 +68,53 @@ final class Setup
 	</style>';
   }
 
-  public static function custom_login_logo_url()
-  {
+  public static function custom_login_logo_url() {
     return home_url();
   }
 
-  public static function custom_login_logo_url_title()
-  {
+  public static function custom_login_logo_url_title() {
     return get_bloginfo('name');
+  }
+
+  public static function disable_comments() {
+    add_action('admin_init', function () {
+      // Redirect any user trying to access comments page
+      global $pagenow;
+
+      if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+      }
+
+      // Remove comments metabox from dashboard
+      remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+      // Disable support for comments and trackbacks in post types
+      foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+          remove_post_type_support($post_type, 'comments');
+          remove_post_type_support($post_type, 'trackbacks');
+        }
+      }
+    });
+
+    // Close comments on the front-end
+    add_filter('comments_open', '__return_false', 20, 2);
+    add_filter('pings_open', '__return_false', 20, 2);
+
+    // Hide existing comments
+    add_filter('comments_array', '__return_empty_array', 10, 2);
+
+    // Remove comments page in menu
+    add_action('admin_menu', function () {
+      remove_menu_page('edit-comments.php');
+    });
+
+    // Remove comments links from admin bar
+    add_action('init', function () {
+      if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+      }
+    });
   }
 }
